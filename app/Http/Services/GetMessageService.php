089e84e0ba0d5d870c9511b69a3cb139460bf8a2
jojoarianto <irianto94@outlook.com>
2018-02-13 15:57:03 +0700

    try 9

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -4,6 +4,7 @@ namespace App\Http\Services;
 
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
 class GetMessageService
 {
@@ -24,7 +25,10 @@ class GetMessageService
         $this->client = new CurlHTTPClient("W8tQwPykagJeHU19x2GoqkiCj9l9P6ofSdfA2TWGvfwucMcnVXecX59aBs7lqfU45OjRtpkABbqZNgqDQk8KTTZEaNHJf/MWKENVWaFkj3q2sO/2eiOu7xa739J4LGsHmSZP8k8UnAyEOTExEdisvgdB04t89/1O/w1cDnyilFU=");
         $this->bot = new LINEBot($this->client, ['channelSecret' => "7f103fc401ae13302013413730f6b606" ]);
         
-        $response = $this->bot->replyText($replyToken, 'hello!');
+        $imageMessageBuilder = new ImageMessageBuilder('https://cybercampus.unair.ac.id/foto_mhs/081611633032.JPG', 'https://cybercampus.unair.ac.id/foto_mhs/081611633032.JPG');
+        // $bot->replyMessage($replyToken, $imageMessageBuilder);
+
+        $response = $this->bot->replyText($replyToken, $imageMessageBuilder);
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
