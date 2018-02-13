871632cf3696f200157926d0996edcbbd8b1ce99
jojoarianto <irianto94@outlook.com>
2018-02-13 15:35:14 +0700

    try 8

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
-use Closure;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -15,21 +14,4 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        // Add this:
-        if($request->method() == 'POST')
-        {
-		    return $next($request);
-        }
-    }
 }
