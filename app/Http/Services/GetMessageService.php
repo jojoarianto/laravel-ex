b79a8c7441ec4936e758a781e10de112d74121a4
jojoarianto <irianto94@outlook.com>
2018-02-14 01:09:56 +0700

    try 13

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -24,34 +24,30 @@ class GetMessageService
         
         $this->client = new CurlHTTPClient("W8tQwPykagJeHU19x2GoqkiCj9l9P6ofSdfA2TWGvfwucMcnVXecX59aBs7lqfU45OjRtpkABbqZNgqDQk8KTTZEaNHJf/MWKENVWaFkj3q2sO/2eiOu7xa739J4LGsHmSZP8k8UnAyEOTExEdisvgdB04t89/1O/w1cDnyilFU=");
         $this->bot = new LINEBot($this->client, ['channelSecret' => "7f103fc401ae13302013413730f6b606" ]);
+
+        // $data = json_decode($body, true);
+        $data = $formData;
+        if(is_array($data['events'])){
+            foreach ($data['events'] as $event)
+            {
+                if ($event['type'] == 'message')
+                {
+                    if($event['message']['type'] == 'text')
+                    {
+                        // send same message as reply to user
+                        $result = $bot->replyText($event['replyToken'], $event['message']['text']);
         
-        // $formData['events']['0']['replyToken'];
-        // if(['events']['0']['text'] == '1'){
-        //     $response = $this->bot->replyText($replyToken, "Your input is = 1");        
-        // } 
-        // if(['events']['0']['text'] == '2'){
-        //     $response = $this->bot->replyText($replyToken, "Your input is = 2");        
-        // }
-        // if(['events']['0']['text'] == '3'){
-        //     $imageMessageBuilder = new ImageMessageBuilder('https://laravel-mysql-persistent-katakanunair.a3c1.starter-us-west-1.openshiftapps.com/img/081611633032.jpg', 'https://laravel-mysql-persistent-katakanunair.a3c1.starter-us-west-1.openshiftapps.com/img/081611633032.jpg');
-        //     $response = $this->bot->replyText($replyToken, $imageMessageBuilder);        
-        // }
+                        // or we can use replyMessage() instead to send reply message
+                        // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                        // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+        
+                        // return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+                    }
+                }
+            }
+        }
 
-        // $imageMessageBuilder = new ImageMessageBuilder('https://laravel-mysql-persistent-katakanunair.a3c1.starter-us-west-1.openshiftapps.com/img/081611633032.jpg', 'https://laravel-mysql-persistent-katakanunair.a3c1.starter-us-west-1.openshiftapps.com/img/081611633032.jpg');
-        // $bot->replyMessage($replyToken, $imageMessageBuilder);
-
-        // $response = $this->bot->replyText($replyToken, $imageMessageBuilder);
-
-        // $msg = $formData['events']['0']['messages'];
-
-        // $txt = $msg[0]['text'];
-        // if($txt == "foto"){
-        //     $imageMessageBuilder = new ImageMessageBuilder('https://laravel-mysql-persistent-katakanunair.a3c1.starter-us-west-1.openshiftapps.com/img/081611633032.jpg', 'https://laravel-mysql-persistent-katakanunair.a3c1.starter-us-west-1.openshiftapps.com/img/081611633032.jpg');
-        //     $response = $this->bot->replyText($replyToken, $imageMessageBuilder);
-        // } else {
-        //     $response = $this->bot->replyText($replyToken, "Your input is = '".$txt."'");
-        // }
-            $response = $this->bot->replyText($replyToken, "Your input is = ");
+        $response = $this->bot->replyText($replyToken, "Your input is = ");
         
         
         if ($response->isSucceeded()) {
