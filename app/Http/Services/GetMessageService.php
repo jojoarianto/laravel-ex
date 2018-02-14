66086239ae9f3b534d7b2fe3aaa3c1435079414f
jojoarianto <irianto94@outlook.com>
2018-02-14 10:14:04 +0700

    try 25

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -5,6 +5,7 @@ namespace App\Http\Services;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use App\Mahasiswa;
 
 class GetMessageService
 {
@@ -24,47 +25,51 @@ class GetMessageService
         
         $this->client = new CurlHTTPClient("W8tQwPykagJeHU19x2GoqkiCj9l9P6ofSdfA2TWGvfwucMcnVXecX59aBs7lqfU45OjRtpkABbqZNgqDQk8KTTZEaNHJf/MWKENVWaFkj3q2sO/2eiOu7xa739J4LGsHmSZP8k8UnAyEOTExEdisvgdB04t89/1O/w1cDnyilFU=");
         $this->bot = new LINEBot($this->client, ['channelSecret' => "7f103fc401ae13302013413730f6b606" ]);
-
-        // $data = json_decode($body, true);
-        // $data = $formData;
-        // if(is_array($formData['events'])){
-        //     foreach ($data['events'] as $event)
-        //     {
-        //         if ($event['type'] == 'message')
-        //         {
-        //             if($event['message']['type'] == 'text')
-        //             {
-        //                 // send same message as reply to user
-        //                 $result = $bot->replyText($event['replyToken'], $event['message']['text']);
-        
-        //                 // or we can use replyMessage() instead to send reply message
-        //                 // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
-        //                 // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-        
-        //                 // return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-        //             }
-        //         }
-        //     }
-        // }
-
-        // http://mbot.goprint.id/img/081611633032.jpg
-
-
         
         $msg = $formData['events']['0']['message'];
         logger( $msg['text'] );
 
-        $output = "Download image = " . $msg['text'] . " \nHasil :";
-        // $response = $this->bot->replyText($replyToken, $output);
+        // $output = "Pencarian= " . $msg['text'] . " \nHasil :";
+        
+        $msg['text'] = "Cari jessica";
+        $msg['text'] = strtolower($msg['text']);
+        $array = explode( " ", $msg['text'] );
+        if(count($array) <= 1){
+            // input tidak valid
+        } else {
+            if($array[0] == 'cari'){
+                $t = substr($msg['text'], 5);
+                $output = 'kata kunci : '.$t;
+                $mhs = Mahasiswa::where('nama', 'like', '%' . $t . '%')->take(5)->get();
+                foreach ($mhs as $k => $mh) {
+                    // $output = $output . "<br/> (".$k.") " . $mh->nama;
+                    $output = $output . "\n(".$k.") " . $mh->nama;                    
+                }
+            } else {
+                $output = "perintah yang anda masukkan salah";
+            }
+        }
+        
+        // echo $output;
 
-        // $url = $msg['text'];
-        $url = "https://cybercampus.unair.ac.id/foto_mhs/".$msg['text'].".JPG";
-        $imageMessageBuilder = new LINEBot\MessageBuilder\ImageMessageBuilder($url, $url);
-        $this->bot->replyMessage($replyToken, $imageMessageBuilder);
+        $response = $this->bot->replyText($replyToken, $output);
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
             return;
         }
+    }
+
+    public function getImg()
+    {
+        $url = "https://cybercampus.unair.ac.id/foto_mhs/".$msg['text'].".JPG";
+        $imageMessageBuilder = new LINEBot\MessageBuilder\ImageMessageBuilder($url, $url);
+        $this->bot->replyMessage($replyToken, $imageMessageBuilder);
+    }
+
+    public function cari($string)
+    {
+        
+        return "";
     }
 }
